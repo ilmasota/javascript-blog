5bfdfa3dc1ce57870d09f9eb638389fdfe63faca
Gosia Marczak <uroshirvisoittaa@gmail.com>
2020-08-05 18:43:50 +0200

    Add generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -31,12 +31,6 @@
     targetArticle.classList.add('active');
   }
 
-  const links = document.querySelectorAll('.titles a');
-
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
-
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
@@ -44,19 +38,32 @@
   function generateTitleLinks(){
 
     /* remove contents of titleList */
-
+      const titleList = document.querySelector(optTitleListSelector);
+      titleList.innerHTML='';
     /* for each article */
+      const articles = document.querySelectorAll(optArticleSelector);
 
+      let html = '';
+
+      for(let article of articles){
       /* get the article id */
-
+        const articleId = article.getAttribute('id');
       /* find the title element */
-
       /* get the title from the title element */
-
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
       /* create HTML of the link */
-
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
       /* insert link into titleList */
+        // titleList.insertAdjacentHTML('afterbegin', linkHTML);
+        html = html + linkHTML;
+      }
 
+      titleList.innerHTML = html;
+      const links = document.querySelectorAll('.titles a');
+
+      for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+      }
   }
 
   generateTitleLinks();
